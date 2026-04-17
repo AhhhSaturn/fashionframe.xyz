4919e8fa8d050c59414708be5cef91202e87703f
Saturn Maoudis <saturnmaoudis@protonmail.com>
2026-04-17 19:24:17 +0100

    i hate docker sm

--- packages/data/healthcheck.ts
+++ packages/data/healthcheck.ts
@@ -5,8 +5,10 @@ const api = treaty<API>("localhost:3000");
 
 api.get().then((res) => {
 	if (res.status !== 200) {
+		console.log("not healthy");
 		process.exit(1);
 	} else {
+		console.log("healthy!");
 		process.exit(1);
 	}
 });
